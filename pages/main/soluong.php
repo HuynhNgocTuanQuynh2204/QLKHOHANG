3eaa6e46f09fec7be557ca3c1bbeb6151131eb63
HuynhNgocTuanQuynh2204 <tuanquynh3452@gmail.com>
2024-07-05 18:23:25 +0700

    first name

--- pages/main/soluong.php
+++ pages/main/soluong.php
@@ -1,23 +1,22 @@
 <?php
- 
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM sanpham WHERE id_sp ='".$id."' LIMIT 1";
-    $qr = mysqli_query($mysqli,$sql);
-    $row_sp = mysqli_fetch_array($qr);
+$id = $_GET['id'];
+$sql = "SELECT * FROM sanpham WHERE id_sp ='".$id."' LIMIT 1";
+$qr = mysqli_query($mysqli, $sql);
+$row_sp = mysqli_fetch_array($qr);
 ?>
 
 <link rel="stylesheet" href="css/table.css">
 <main class="main container" id="main">
-    <div class="container" style="margin-left: 100p;">
-    <h6 style="text-align: center; text-transform: uppercase; font-weight: bold;">Tổng số lượng trong kho: <?php echo $row_sp['soluong'] ?></h6>
-    <a href="index.php?quanly=danhmucsanpham" Class="btn btn-success" style="margin-left: 500px;">Nhập số lượng cần xuất </a>
+    <div class="container" style="margin-left: 100px;">
+        <h6 style="text-align: center; text-transform: uppercase; font-weight: bold;">Tổng số lượng trong kho: <?php echo $row_sp['soluong'] ?></h6>
+        <a href="index.php?quanly=quanlixuatkho" class="btn btn-success" style="margin-left: 500px;">Danh sách hàng đã xuất</a>
         <div class="row d-flex justify-content-right align-items-right">
             <div class="col-5">
-                <h1 class="mb-5">Thêm danh mục sản phẩm</h1>
-                <form action="index.php?quanly=xuatkhosanpham&id=<?php echo $row_sp['id_sp']; ?>" method="POST" enctype="multipart/form-data">
+                <h1 class="mb-5">Nhập số lượng cần xuất</h1>
+                <form id="xuatKhoForm" action="index.php?quanly=xuatkhosanpham&id=<?php echo $row_sp['id_sp']; ?>" method="POST" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label class="form-label">Số lượng</label>
-                        <input name="soluong" type="text" class="form-control" required >
+                        <input id="soluong" name="soluong" type="number" class="form-control" required>
                     </div>
                     <button type="submit" class="btn btn-primary" name="xuatkho">Xuất kho</button>
                 </form>
@@ -30,5 +29,14 @@
             </div>
         </div>
     </div>
-
 </main>
+
+<script>
+    document.getElementById('xuatKhoForm').addEventListener('submit', function(event) {
+        var soluong = document.getElementById('soluong').value;
+        if (soluong < 0) {
+            event.preventDefault(); 
+            alert('Số lượng không thể là số âm.');
+        }
+    });
+</script>
